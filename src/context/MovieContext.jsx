8c2aea8aad590a1380287f98c7a30a0b415ab3c4
MicheleFumi <michele.fumi96@gmail.com>
2024-12-11 17:28:26 +0100

    add load and error

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -3,27 +3,37 @@ import { createContext, useEffect, useState } from "react";
 const MovieContext = createContext()
 
 export default function MovieProvider({ children }) {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     const [movieDataApi, setMovieDataApi] = useState({})
     const url = 'http://localhost:3001/'
     const endpoint = 'api/movies/'
     function fetchMovieData() {
         fetch(`${url}${endpoint}`)
-            .then(resp => resp.json())
-            .then(data => {
-                console.log(data);
-
-                setMovieDataApi(data)
-
-
-            }
-            )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Errore HTTP: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setMovieDataApi(data);
+            })
+            .catch((err) => {
+                setError(err.message);
+                console.error("Errore nel fetch:", err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     }
+
 
     useEffect(fetchMovieData, [])
 
     console.log("MovieProvider rendering, movieDataApi:", movieDataApi)
     return (
-        <MovieContext.Provider value={{ movieDataApi }}>
+        <MovieContext.Provider value={{ movieDataApi, loading, error }}>
             {children}
         </MovieContext.Provider>
     );
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -4,75 +4,83 @@ import Jumbotron from "../components/Jumbotron";
 import MovieCard from "../components/MovieCard";
 
 export default function MoviesPage() {
-    const movies = [
-        {
-            id: 1,
-            titolo: "Pulp Fiction",
-            regista: "Quentin Tarantino",
-            anno: 1994
-        },
-        {
-            id: 2,
-            titolo: "Il padrino",
-            regista: "Francis Ford Coppola",
-            anno: 1972
-        },
-        {
-            id: 3,
-            titolo: "Inception",
-            regista: "Christopher Nolan",
-            anno: 2010
-        },
-        {
-            id: 4,
-            titolo: "Fight Club",
-            regista: "David Fincher",
-            anno: 1999
-        },
-        {
-            id: 5,
-            titolo: "Matrix",
-            regista: "Lana Wachowski, Lilly Wachowski",
-            anno: 1999
-        },
-        {
-            id: 6,
-            titolo: "C'era una volta in America",
-            regista: "Sergio Leone",
-            anno: 1984
-        },
-        {
-            id: 7,
-            titolo: "Forrest Gump",
-            regista: "Robert Zemeckis",
-            anno: 1994
-        },
-        {
-            id: 8,
-            titolo: "Il cavaliere oscuro",
-            regista: "Christopher Nolan",
-            anno: 2008
-        },
-        {
-            id: 9,
-            titolo: "La La Land",
-            regista: "Damien Chazelle",
-            anno: 2016
-        },
-        {
-            id: 10,
-            titolo: "Parasite",
-            regista: "Bong Joon-ho",
-            anno: 2019
-        }
-    ];
+    /*  const movies = [
+         {
+             id: 1,
+             titolo: "Pulp Fiction",
+             regista: "Quentin Tarantino",
+             anno: 1994
+         },
+         {
+             id: 2,
+             titolo: "Il padrino",
+             regista: "Francis Ford Coppola",
+             anno: 1972
+         },
+         {
+             id: 3,
+             titolo: "Inception",
+             regista: "Christopher Nolan",
+             anno: 2010
+         },
+         {
+             id: 4,
+             titolo: "Fight Club",
+             regista: "David Fincher",
+             anno: 1999
+         },
+         {
+             id: 5,
+             titolo: "Matrix",
+             regista: "Lana Wachowski, Lilly Wachowski",
+             anno: 1999
+         },
+         {
+             id: 6,
+             titolo: "C'era una volta in America",
+             regista: "Sergio Leone",
+             anno: 1984
+         },
+         {
+             id: 7,
+             titolo: "Forrest Gump",
+             regista: "Robert Zemeckis",
+             anno: 1994
+         },
+         {
+             id: 8,
+             titolo: "Il cavaliere oscuro",
+             regista: "Christopher Nolan",
+             anno: 2008
+         },
+         {
+             id: 9,
+             titolo: "La La Land",
+             regista: "Damien Chazelle",
+             anno: 2016
+         },
+         {
+             id: 10,
+             titolo: "Parasite",
+             regista: "Bong Joon-ho",
+             anno: 2019
+         }
+     ]; */
 
-    /*  const { movieDataApi } = useContext(MovieContext);
-     console.log(movieDataApi); */
+    const { movieDataApi, loading, error } = useContext(MovieContext);
+    console.log(movieDataApi);
 
-    /*  const movies = movieDataApi?.movies || [];
-     console.log(movies);
-  */
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Errore: {error}</div>;
+    }
+
+
+
+
     return (
         <>
             <Jumbotron title='The Movie Forum' subtitle='discover new film, suggested by people' description='Lorem ipsum dolor sit amet consectetur adipisicing elit.' />
@@ -81,7 +89,7 @@ export default function MoviesPage() {
                     <h2 className="pb-3">Movie Overview</h2>
                     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
 
-                        {movies.map(movie => (
+                        {movieDataApi.map(movie => (
                             <div className="col" key={movie.id}>
                                 <MovieCard movie={movie} />
                             </div>
