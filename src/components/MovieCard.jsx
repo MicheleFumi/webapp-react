3461a5e29c6828db5cb0169ad297915bf62ebe0b
MicheleFumi <michele.fumi96@gmail.com>
2024-12-11 14:44:56 +0100

    add movie list and show in page

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+export default function MovieCard({ movie }) {
+    return (
+
+        <>
+            <div className="book card">
+                <div className="card-body">
+                    <h4>{movie.titolo}</h4>
+                    <span className="text-muted">By <span>{movie.regista}</span></span>
+                    <p className="overview mb-3">
+                        {movie.anno}
+                    </p>
+                    <Link to={`/books/${movie.id}`} className="btn btn-secondary btn-outline-dark">Read more</Link>
+                </div>
+            </div>
+        </>
+
+    )
+}
--- src/pages/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage.jsx
@@ -1,6 +1,8 @@
 import Jumbotron from "../components/Jumbotron";
 
 export default function SingleMoviePage() {
+
+
     return (
         <>
             <Jumbotron title='The Movie Forum' subtitle='discover new film, suggested by people' description='discover what people says about.' />
